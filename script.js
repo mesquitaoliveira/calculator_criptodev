a287d5fb8a85c42a974d7de8d8072ef960652348
Mesquita Oliveira <ismael.mesquita.100@gmail.com>
2022-04-26 20:41:30 -0300

    min code

---
+++ script.js
@@ -0,0 +1,13 @@
+function c(val) {
+  document.getElementById("display").value = val;
+}
+function v(val) {
+  document.getElementById("display").value += val;
+}
+function e() {
+  try {
+    c(eval(document.getElementById("display").value));
+  } catch (e) {
+    c("Error");
+  }
+}
